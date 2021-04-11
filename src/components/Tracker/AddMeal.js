92564ca889d0edb612d01de6b5ecb39eb17602ee
Nick Schuster <ngpnickschster@gmail.com>
2021-04-11 01:22:22 -0700

    Adding ingredients to form.

--- src/components/Tracker/AddMeal.js
+++ src/components/Tracker/AddMeal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   makeStyles,
@@ -71,6 +71,34 @@ const useStyles = makeStyles((theme) => ({
 
 export const AddMeal = ({ setTrackerState, addMeal }) => {
   const classes = useStyles();
+  const [meal, setMeal] = useState({
+    name: "",
+    ingredients: [
+      {
+        key: Math.random(),
+        name: "",
+        calories: "",
+        protein: "",
+        fat: "",
+        carbs: "",
+      },
+    ],
+  });
+
+  // Add a new empty ingredient.
+  const addIngredient = () => {
+    setMeal((prev) => {
+      prev.ingredients.push({
+        key: Math.random(),
+        name: "",
+        calories: "",
+        protein: "",
+        fat: "",
+        carbs: "",
+      });
+      return { ...prev };
+    });
+  };
 
   return (
     <>
@@ -86,8 +114,9 @@ export const AddMeal = ({ setTrackerState, addMeal }) => {
               <TextField
                 className={classes.input1}
                 id="outlined"
-                label="Meal name"
+                label="Meal"
                 variant="outlined"
+                placeholder="e.g. Lunch"
               />
             </Grid>
             <Grid item xs={2}>
@@ -102,82 +131,88 @@ export const AddMeal = ({ setTrackerState, addMeal }) => {
             </Grid>
           </Grid>
           <div className={`${classes.seperator} ${classes.formItem}`} />
-          <Grid
-            container
-            justify="space-between"
-            alignItems="center"
-            className={classes.formItem}
-          >
-            <Grid item xs={6}>
-              <TextField
-                className={classes.input1}
-                id="outlined"
-                label="Ingredient name"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <IconButton color="primary">
-                <SaveOutlined />
-              </IconButton>
-            </Grid>
-            <Grid item xs={2}>
-              <IconButton color="primary">
-                <InsertDriveFileOutlined />
-              </IconButton>
-            </Grid>
-            <Grid item xs={2}>
-              <IconButton color="primary">
-                <Close />
-              </IconButton>
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            className={classes.formItem}
-            justify="space-evenly"
-            spacing={1}
-          >
-            <Grid item xs={6} lg={3}>
-              <TextField
-                className={classes.calories}
-                id="outlined"
-                label="Calories"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={6} lg={3}>
-              <TextField
-                className={classes.protein}
-                id="outlined"
-                label="Protein"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={6} lg={3}>
-              <TextField
-                className={classes.fat}
-                id="outlined"
-                label="Fat"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={6} lg={3}>
-              <TextField
-                className={classes.carbs}
-                id="outlined"
-                label="Carbs"
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
+          {meal.ingredients.map((ing) => (
+            <div>
+              <Grid
+                key={ing.key}
+                container
+                justify="space-between"
+                alignItems="center"
+                className={classes.formItem}
+              >
+                <Grid item xs={6}>
+                  <TextField
+                    className={classes.input1}
+                    id="outlined"
+                    label="Ingredient"
+                    variant="outlined"
+                    placeholder="e.g. Banana"
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <IconButton color="primary">
+                    <SaveOutlined />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={2}>
+                  <IconButton color="primary" aria-label="">
+                    <InsertDriveFileOutlined />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={2}>
+                  <IconButton color="primary">
+                    <Close />
+                  </IconButton>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                className={classes.formItem}
+                justify="space-evenly"
+                spacing={1}
+              >
+                <Grid item xs={6} lg={3}>
+                  <TextField
+                    className={classes.calories}
+                    id="outlined"
+                    label="Calories"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6} lg={3}>
+                  <TextField
+                    className={classes.protein}
+                    id="outlined"
+                    label="Protein"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6} lg={3}>
+                  <TextField
+                    className={classes.fat}
+                    id="outlined"
+                    label="Fat"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={6} lg={3}>
+                  <TextField
+                    className={classes.carbs}
+                    id="outlined"
+                    label="Carbs"
+                    variant="outlined"
+                  />
+                </Grid>
+              </Grid>
+            </div>
+          ))}
           <Grid
             container
             className={classes.formItem}
             alignItems="center"
             justify="center"
           >
-            <IconButton color="primary">
+            <IconButton color="primary" onClick={() => addIngredient()}>
               <Add />
             </IconButton>
           </Grid>
