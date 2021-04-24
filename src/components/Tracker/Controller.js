e5e3b78146c0087198810a2cd861182648a054a7
Nick Schuster <ngpnickschster@gmail.com>
2021-04-23 17:03:58 -0700

    Day controlls and totals.

--- src/components/Tracker/Controller.js
+++ src/components/Tracker/Controller.js
@@ -7,6 +7,7 @@ import { Totals } from "./Totals";
 import { MealControl } from "./MealControl";
 import { TrackerStates } from "./TrackerStates";
 import { Keygen } from "../../util/keygen";
+import moment from "moment";
 
 const useStyles = makeStyles((theme) => ({
   mealsContainer: {
@@ -31,26 +32,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const TrackerController = () => {
+export const TrackerController = ({ user }) => {
   const classes = useStyles();
   const [trackerState, setTrackerStateHook] = useState(undefined);
   const [meals, setMeals] = useState([]);
+  const [currentDay, setCurrentDay] = useState(moment().unix() / 86400);
   const [savedEditMeal, setEditMeal] = useState(undefined);
+
+  // Set the meals of the current day.
+  useEffect(() => {}, [currentDay]);
+
+  // Save the meal changes to the user.
+  useEffect(() => {}, [meals]);
+
+  // Increase the current day by one.
+  const nextDay = () => {
+    setCurrentDay((prev) => {
+      return prev + 1;
+    });
+  };
+
+  // Decrease the current day by one.
+  const prevDay = () => {
+    setCurrentDay((prev) => {
+      return prev - 1;
+    });
+  };
 
   // Calculate the total for a meal for a given type.
   const getMealTotal = (meal, type) => {
     let total = 0;
     for (const ing of Object.entries(meal.ingredients)) {
-      total += parseInt(ing[1][type]);
+      total += parseInt(ing[1][type], 10);
     }
     return total;
   };
 
   // Calculate the total for all meals for a given type.
-  const getTotals = (meals, type) => {
+  const getTotals = (type) => {
     let total = 0;
     for (const meal of meals) {
-      total += getMealTotal(meal, type);
+      total += getMealTotal(meal.content, type);
     }
     return total;
   };
@@ -79,6 +101,7 @@ export const TrackerController = () => {
     });
   };
 
+  // Replace the current meal content with the updated content.
   const editMeal = (editMeal) => {
     setMeals((prev) => {
       const update = prev.find((meal) => meal.key === savedEditMeal.key);
@@ -96,6 +119,7 @@ export const TrackerController = () => {
     });
   };
 
+  // Detemine what form to show based on tracker state.
   const activeForm = () => {
     if (trackerState === TrackerStates.addMeal) {
       return (
@@ -115,7 +139,7 @@ export const TrackerController = () => {
 
   return (
     <>
-      <Header />
+      <Header currentDay={currentDay} nextDay={nextDay} prevDay={prevDay} />
       <Container className={classes.mealsContainer}>
         <Meals
           setTrackerState={setTrackerState}
@@ -124,7 +148,7 @@ export const TrackerController = () => {
           deleteMeal={deleteMeal}
           saveEditMeal={saveEditMeal}
         />
-        <Totals />
+        <Totals getTotals={getTotals} goals={{}} getPoints={() => "----"} />
         <Footer />
       </Container>
       <div className={classes.formContainer}>{activeForm()}</div>
