171b92b08cff4c49c0ac252e46daca29fb6893c8
Nick Schuster <ngpnickschster@gmail.com>
2021-08-06 16:50:26 -0700

    Lamdba resolver and schema changes.

---
+++ amplify/backend/function/satiatePublicProfile/src/index.js
@@ -0,0 +1,27 @@
+/* Amplify Params - DO NOT EDIT
+	API_SATIATE_DAYTABLE_ARN
+	API_SATIATE_DAYTABLE_NAME
+	API_SATIATE_GRAPHQLAPIENDPOINTOUTPUT
+	API_SATIATE_GRAPHQLAPIIDOUTPUT
+	API_SATIATE_PARAMETERTABLE_ARN
+	API_SATIATE_PARAMETERTABLE_NAME
+	API_SATIATE_USERTABLE_ARN
+	API_SATIATE_USERTABLE_NAME
+	AUTH_SATIATE_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
