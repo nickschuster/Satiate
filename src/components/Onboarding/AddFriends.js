c2e54cf800632d4ddd15ad1a2ffe7af64bce875c
Nick Schuster <ngpnickschster@gmail.com>
2021-08-22 15:12:12 -0700

    Find friends visuals.

--- src/components/Onboarding/AddFriends.js
+++ src/components/Onboarding/AddFriends.js
@@ -5,39 +5,68 @@ import {
   Button,
   Typography,
 } from "@material-ui/core";
-import React from "react";
-import { ImagePlaceholder } from "../../images/placeholder.png";
+import React, { useState } from "react";
+import ImagePlaceholder from "../../images/placeholder.png";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: 20,
+    width: 400,
+    maxWidth: "90vw",
+  },
   input: {
     width: "100%",
   },
   itemContainer: {
     maxHeight: "300px !important",
     overflowY: "auto",
+    marginTop: 10,
   },
   item: {
     width: "100%",
-    height: 100,
-    border: "1px solid black",
+    height: 75,
   },
   itemImage: {
     backgroundImage: `url(${ImagePlaceholder})`,
     backgroundSize: "cover",
     width: 50,
     height: 50,
-    borderRadius: 15,
-    border: "1px solid black",
+    margin: 11,
+    borderRadius: 50,
+    border: "2px solid",
+    borderColor: theme.palette.text.main,
+    float: "left",
+  },
+  itemName: {
+    float: "left",
+    maxWidth: 70,
+    maxHeight: "20px",
+    textOverflow: "ellipsis",
+    overflow: "hidden",
+    marginTop: 25,
+    whiteSpace: "nowrap",
+  },
+  itemFollow: {
+    marginTop: 12,
+    float: "right",
+  },
+  buttonContainer: {
+    marginTop: 10,
   },
 }));
 
 export const AddFriends = () => {
   const classes = useStyles();
 
-  const items = [
+  const [items, setItems] = useState([
     {
       image: "test",
       name: "test",
+      following: false,
+    },
+    {
+      image: "test",
+      name: "testasdfsssss asdf asdf asdf",
       following: false,
     },
     {
@@ -55,17 +84,19 @@ export const AddFriends = () => {
       name: "test",
       following: false,
     },
-    {
-      image: "test",
-      name: "test",
-      following: false,
-    },
-  ];
+  ]);
+
+  const followProfile = (key) => {
+    setItems((prev) => {
+      prev[key].following = !prev[key].following;
+      return [...prev];
+    });
+  };
 
   return (
     <>
       <form>
-        <Grid container direction="column">
+        <Grid container direction="column" className={classes.container}>
           <Grid item xs={12}>
             <TextField
               className={classes.input}
@@ -78,15 +109,19 @@ export const AddFriends = () => {
               // helperText={formErrors.email}
             />
           </Grid>
-          <Grid item className={classes.itemContainer} alignItems="center">
+          <Grid item className={classes.itemContainer}>
             {items.map((value, key) => (
               <div className={classes.item} key={key}>
                 {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                <img src={value.image} className={classes.itemPicture} />
+                <img src={value.image} className={classes.itemImage} />
                 <div className={classes.itemName}>{value.name}</div>
                 <div className={classes.itemFollow}>
-                  <Button>
-                    <Typography>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={() => followProfile(key)}
+                  >
+                    <Typography variant="body1">
                       {value.following ? "Unfollow" : "Follow"}
                     </Typography>
                   </Button>
@@ -94,15 +129,19 @@ export const AddFriends = () => {
               </div>
             ))}
           </Grid>
-          <Grid container>
+          <Grid
+            container
+            justify="space-evenly"
+            className={classes.buttonContainer}
+          >
             <Grid item>
-              <Button>
-                <Typography>Back</Typography>
+              <Button variant="outlined" color="primary">
+                <Typography variant="h5">Back</Typography>
               </Button>
             </Grid>
             <Grid item>
-              <Button>
-                <Typography>Next</Typography>
+              <Button variant="contained" color="primary">
+                <Typography variant="h5">Next</Typography>
               </Button>
             </Grid>
           </Grid>
