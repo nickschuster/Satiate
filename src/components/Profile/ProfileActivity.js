2012556b81ddaebc71840132ae7851391075b8df
Nick Schuster <ngpnickschster@gmail.com>
2021-09-14 17:50:45 -0700

    Added formating for custom calendar.

--- src/components/Profile/ProfileActivity.js
+++ src/components/Profile/ProfileActivity.js
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 import moment from "moment";
 
 import "react-calendar-heatmap/dist/styles.css";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   calendar: {
     marginTop: 20,
     marginBottom: 20,
   },
   col: {
-    float: "left",
-    width: 62,
-    border: "1px solid black",
+    height: 64,
   },
   row: {
     height: 50,
     width: 50,
     margin: 5,
-    border: "1px solid black",
+    border: "2px solid",
+    borderColor: theme.palette.secondary.main,
+    float: "left",
+    borderRadius: 100,
+    textAlign: "center",
   },
-});
+  value1: {
+    backgroundColor: theme.palette.text.main + "0C",
+  },
+  value2: { backgroundColor: theme.palette.text.main + "7F" },
+  value3: { backgroundColor: theme.palette.text.main + "BF" },
+  value4: { backgroundColor: theme.palette.text.main + "FF" },
+  notActive: {
+    opacity: 0.5,
+  },
+  rowTitle: {
+    marginTop: 14,
+    color: theme.palette.secondary.main,
+    fontWeight: "bold",
+  },
+}));
 
 export const ProfileActivity = () => {
   const classes = useStyles();
@@ -32,41 +48,78 @@ export const ProfileActivity = () => {
     setValues(getValues());
   }, []);
 
-  // Calculate the start date for the heatmap.
-  const getStartDate = () => {
-    return getShiftedDate(30, endDate);
-  };
-
   // Shift a date by an amount of days.
   const getShiftedDate = (amount, date) => {
     return moment(date).subtract(amount, "days");
   };
 
+  // Populate the calendar with the previous 30 days.
   const getValues = () => {
     const values = [];
+    let index = 0;
     for (let i = 0; i < 5; i++) {
       values.push([]);
       for (let j = 0; j < 6; j++) {
-        console.log(i * 6 + j);
         values[i].push({
-          date: getShiftedDate(i * 5 + j, endDate),
-          value: 0,
+          date: getShiftedDate(index, endDate),
+          value: getValueAmount(),
         });
+        index++;
       }
+      values[i].reverse();
     }
+    values.reverse();
     return values;
+  };
+
+  // Get the string representation of the currently displayed month.
+  const getCurrentMonth = () => {
+    return endDate.format("MMMM");
+  };
+
+  // Get the score for a particular date.
+  const getValueAmount = (date) => {
+    return Math.floor(Math.random() * 5);
+  };
+
+  // Get the color class for the respective value.
+  const getValueColor = (value) => {
+    if (value == 1) return classes.value1;
+    if (value == 2) return classes.value2;
+    if (value == 3) return classes.value3;
+    if (value == 4) return classes.value4;
+    return "";
+  };
+
+  // Determine if this date is part of the currently selected month.
+  // Return non active class if it is not.
+  const isPartOfActiveMonth = (date) => {
+    if (date.format("MM") !== endDate.format("MM")) return classes.notActive;
+    return "";
   };
 
   return (
     <>
       <Grid container justify="center" alignItems="center">
         <Grid item>
+          <div>
+            <Typography variant="h5" color="secondary">
+              {getCurrentMonth()}
+            </Typography>
+          </div>
           <div className={classes.calendar}>
             {values.map((col, colIndex) => (
               <div className={classes.col} key={colIndex}>
                 {col.map((row, rowIndex) => (
-                  <div className={classes.row} key={rowIndex * (colIndex + 1)}>
-                    {row.date.format("DD")}
+                  <div
+                    className={`${classes.row} ${getValueColor(
+                      row.value
+                    )} ${isPartOfActiveMonth(row.date)}`}
+                    key={rowIndex * (colIndex + 1)}
+                  >
+                    <div className={classes.rowTitle}>
+                      {row.date.format("D")}
+                    </div>
                   </div>
                 ))}
               </div>
